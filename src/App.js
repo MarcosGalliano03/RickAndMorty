3eb4bac9d04f06940fb1e8d207858996ce6e6ff4
marcos galliano <marquitosgalliano@hotmail.com>
2023-04-28 19:37:47 -0300

    new repository

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import Form from './components/Forms';
+import Cards from './components/cards/Cards.jsx';
+import About from './components/About'
+import Detail from './components/Detail';
+import Nav from './components/Nav';
+import { useState } from 'react';
+import { Route, Routes, useLocation } from 'react-router-dom';
+import axios from 'axios';
+import Favorites from './components/Favorites';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   const navigate = useNavigate();
+   const [characters, setCharacters] = useState([])
+   const [access, setAccess] = useState(false);
+
+   let EMAIL = "marquitosgalliano@hotmail.com";
+   let PASSWORD = "abc123"; 
+
+
+   function login(userData) {
+      if (userData.password === PASSWORD && userData.email === EMAIL) {
+         setAccess(true);
+         navigate('/home');
+      }
+   }
+
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   const location = useLocation();
+
+   function onSearch(id) {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+         if (data.name) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            window.alert('¡No hay personajes con este ID!');
+         }
+      });
+   }
+
+   function onClose(id) {
+      const filteredCharacters = characters.filter((character) => character.id !== parseInt(id));
+      setCharacters(filteredCharacters);
+      console.log("holis");
+      console.log(id);
+   }
+
+   return (
+      <div className={`App${location.pathname === '/' ? ' home' : ''}`}>
+         {location.pathname !== '/' && <Nav onSearch={onSearch} />}
+         <Routes>
+            <Route path='/favorites' element={<Favorites/>}/>
+            <Route exact path="/" element={<Form login={login}/>} /> //modificar el fondo de la app en este route
+            <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/detail/:id" element={<Detail />} />
+         </Routes>
+      </div>
+   );
 }
 
 export default App;
